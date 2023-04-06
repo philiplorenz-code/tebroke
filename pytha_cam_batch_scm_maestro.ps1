6416336ab1126c48befd83e367f61ef3bfec1e66
Philip Lorenz <philip@learning-it.io>
2023-04-06 22:02:39 +0200

    rebuilt

--- pytha_cam_batch_scm_maestro.ps1
+++ pytha_cam_batch_scm_maestro.ps1
@@ -97,7 +97,7 @@ function Search-Array {
   Write-Log -Message "Diese Ergebnisse wurden gefunden:" -LogType "Info"
 
   foreach ($line in $text) {
-    if ($line -match $searchkey) {
+    if ($line -like $searchkey) {
       Write-Log -Message "$line" -LogType "Info"
       $results.Add($line) | Out-Null
     }
@@ -201,19 +201,17 @@ function Replace-CreateBladeCut([string]$Filename) {
   $Content = Get-Content $Filename
   Write-Log -Message "Replace-CreateBladeCut" -LogFilePath $logfile
   # Prob: Die Zeilen werden nicht eingefügt 
-  # Add Lines Befores
-  #
-
+  # Add Lines Before
   $Array = @()
   $Array += 'SetApproachStrategy(true, true, 0.8);'
   $Array += 'SetRetractStrategy(true, true, 0.8, 0);'
   $Array += 'CreateSectioningMillingStrategy(3, 150, 0);'
 
-  #
-  $KeyWord = Search-Array -text $Content -searchkey 'CreateBladeCut("SlantedBladeCut*", "", TypeOfProcess.GeneralRouting,*, "-1",*, 2);'
+  $KeyWord = Search-Array -text $Content -searchkey 'CreateBladeCut("SlantedBladeCut1", "", TypeOfProcess.GeneralRouting,*, "-1",*, 2);'
   Write-Log -Message "KeyWord: $KeyWord" -LogFilePath $logfile
   if ($KeyWord) {
-
+    Write-Log -Message "Funktion: Replace-CreateBladeCut" -LogFilePath $logfile
+    Write-Log -Message "if (KeyWord) hat angeschlagen" -LogFilePath $logfile
     foreach ($kw in $KeyWord) {
       Write-Log -Message "vor $kw wird nun ergänzt." -LogFilePath $logfile
       Add-StringBefore -insert $Array -keyword $kw -textfile $Filename
